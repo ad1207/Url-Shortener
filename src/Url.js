9798ec5b3a2d02595f23428f991d6ac5bf48a075
Adithya R <59307022+ad1207@users.noreply.github.com>
2022-08-05 19:38:51 +0530

    Update Url.js

--- src/Url.js
+++ src/Url.js
@@ -6,7 +6,7 @@ export default function Url() {
     const urlParams = useParams()
     useEffect(() => {
         const fetch = async () => {
-            let res = await axios.get("http://localhost:5000/"+urlParams.code)
+            let res = await axios.get("https://url-shortener--nodejs.herokuapp.com/"+urlParams.code)
             console.log(res.data)
             window.location.replace(res.data)
         }
